b6113930b96371c4490dab0245285767bc914de2
Li Qiaopeng <qiaopeng.li@smartmore.com>
2022-08-17 14:44:09 +0800

    feat: modify fence example

--- 7_fence.cpp
+++ 7_fence.cpp
@@ -1,43 +1,99 @@
-#include <assert.h>
-
-#include <atomic>
 #include <iostream>
 #include <thread>
-using namespace std;
-atomic<int> z;
-atomic<bool> x, y;
+#include <atomic>
 
-void write_x_then_y()
-{
-    // atomic_thread_fence(memory_order_release);
-    x.store(true, memory_order_relaxed);
-    y.store(true, memory_order_relaxed);
+int x = 0;
+int y = 0;
+
+int r0 = 100;
+int r1 = 100;
+
+void f1() {
+    x = 1;
+    std::atomic_thread_fence(std::memory_order_release);
+    r0 = y;
 }
 
-void read_y_then_x()
-{
-    while (!y.load(memory_order_relaxed));
-    // atomic_thread_fence(memory_order_acquire);
-    if (x.load(memory_order_relaxed)) ++z;
+void f2() {
+    y = 1;
+    std::atomic_thread_fence(std::memory_order_acquire);
+    r1 = x;
 }
 
-void test()
-{
-    x = false;
-    y = false;
-    z = 0;
-    thread t1(write_x_then_y);
-    thread t2(read_y_then_x);
-    t1.join();
-    t2.join();
-    assert(z.load() != 0);
+void init() {
+    x = 0;
+    y = 0;
+    r0 = 100;
+    r1 = 100;
 }
-int main()
-{
-    for (long int i = 0; i < 1e6+1; i++)
-    {
-        test();
-        if (i % 10000 == 0) cout << i << " time OK" << endl;
+
+bool check() {
+    return r0 == 0 && r1 == 0;
+}
+
+// 原子变量
+std::atomic<bool> wait1{true}; 
+std::atomic<bool> wait2{true};  
+std::atomic<bool> stop{false};
+
+void loop1() {
+    while(!stop.load(std::memory_order_relaxed)) {
+        // 等到wait1被赋值false, 才进行操作
+        while (wait1.load(std::memory_order_relaxed)){}; 
+
+        asm volatile("" ::: "memory");  // 阻止编译器优化
+        f1();
+        asm volatile("" ::: "memory");
+
+        wait1.store(true, std::memory_order_relaxed);
     }
+}
+
+void loop2() {
+    while (!stop.load(std::memory_order_relaxed)) {
+        while (wait2.load(std::memory_order_relaxed));
+
+        asm volatile("" ::: "memory");
+        f2();
+        asm volatile("" ::: "memory");
+
+        wait2.store(true, std::memory_order_relaxed);
+    }
+}
+
+int main() {
+    std::thread thread1(loop1);
+    std::thread thread2(loop2);
+
+    long count = 0;
+    while(true) {
+        count++;
+        init();
+
+        asm volatile("" ::: "memory");
+        // 线程执行f函数
+        wait1.store(false, std::memory_order_relaxed);
+        wait2.store(false, std::memory_order_relaxed);
+
+        // 线程执行完
+        while (!wait1.load(std::memory_order_relaxed));
+        while (!wait2.load(std::memory_order_relaxed));
+        asm volatile("" ::: "memory");
+        
+        if (check()) {
+            std::cout << "test count " << count << ": r0 == " << r0 << " && r1 == " << r1 << std::endl;
+            break;
+        } else {
+            if (count % 10000 == 0) {
+                std::cout << "test count " << count << ": OK" << std::endl;
+            }
+        }
+    }
+
+    stop.store(true);
+    wait1.store(false);
+    wait2.store(false);
+    thread1.join();
+    thread2.join();
     return 0;
 }
